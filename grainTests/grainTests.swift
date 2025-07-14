9cec162f2a2f11f276f1b09139615098243365dc
larralapid <larralapid@gmail.com>
2025-07-14 01:44:53 -0400

    Initial Commit

---
+++ grainTests/grainTests.swift
@@ -0,0 +1,16 @@
+//
+//  grainTests.swift
+//  grainTests
+//
+//  Created by Larra Lapid on 7/14/25.
+//
+
+import Testing
+
+struct grainTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
---
+++ grainUITests/grainUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  grainUITestsLaunchTests.swift
+//  grainUITests
+//
+//  Created by Larra Lapid on 7/14/25.
+//
+
+import XCTest
+
+final class grainUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
